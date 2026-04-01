5c7ba86fd5139c76a4d31def1e45b074f96e6416
jdjpros <jdj0225@gmail.com>
2026-04-01 21:42:26 +0900

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "장로 고시 학습",
+  "short_name": "장로고시",
+  "description": "장로교 장로 고시 학습 앱",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#1e3a5f",
+  "icons": [
+    {
+      "src": "https://raw.githubusercontent.com/twitter/twemoji/master/assets/72x72/1f4d6.png",
+      "sizes": "72x72",
+      "type": "image/png"
+    },
+    {
+      "src": "https://raw.githubusercontent.com/twitter/twemoji/master/assets/72x72/1f4d6.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
